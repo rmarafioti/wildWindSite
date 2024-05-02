5482cf6438d3e913a7c3f084c27bdee8a57bf8e2
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-05-01 20:55:30 -0500

    created photo gallery with function to view all photos via button click

--- src/layout/Tattoos.jsx
+++ src/layout/Tattoos.jsx
@@ -1,10 +1,28 @@
+import React, { useState } from "react";
 import { tattooPhotos } from "../tattooPhotos";
 
 export default function Tattoos() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleClick = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % tattooPhotos.length);
+  };
+
+  const currentImageObj =
+    tattooPhotos.length > 0 ? tattooPhotos[currentIndex] : null;
+
+  const imageurl = currentImageObj
+    ? new URL(currentImageObj.image, import.meta.url).href
+    : "";
+
   return (
     <>
       <h1>TATTOOS</h1>
       <p>artist contact email & socials</p>
+      <div>
+        <img src={imageurl} alt="tattoo portfolio images" />
+        <button onClick={handleClick}>browse tattoos</button>
+      </div>
     </>
   );
 }
