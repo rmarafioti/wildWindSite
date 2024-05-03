9729f93d6e2909c79a90c425cef7f5ddf9fef37f
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-05-02 19:29:25 -0500

    added content to contact page

--- src/layout/Contact.jsx
+++ src/layout/Contact.jsx
@@ -1,8 +1,18 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
 export default function Contact() {
   return (
     <>
       <h1>CONTACT</h1>
-      <p>address / phone / email / socials</p>
+      <p>
+        Wild Wind Tattoo is located at 1452 N. Western Ave. Chicago, IL 60622
+      </p>
+      <p>PHONE: 773.227.2027 </p>
+      <p>EMAIL: wildwindtattoo@gmail.com</p>
+      <h1>
+        <FaFacebookSquare /> <FaInstagram />
+      </h1>
     </>
   );
 }
